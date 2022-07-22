229c37c43a8f4c1f87d88f596749a915c2598eb2
kevinzhow <kevinchou.c@gmail.com>
2022-07-22 13:31:10 +0800

    fix(replaceAll): polyfill

--- src/main/assets/action/paragraph.js
+++ src/main/assets/action/paragraph.js
@@ -1,8 +1,3 @@
-if (typeof String.prototype.replaceAll === "undefined") {
-  String.prototype.replaceAll = function (match, replace) {
-    return this.replace(new RegExp(match, "g"), () => replace);
-  };
-}
 /**
  * Request Host app to mark paragraph as read
  */
@@ -84,6 +79,12 @@ function addStyle(styleText) {
 }
 
 window.onload = () => {
+  if (typeof String.prototype.replaceAll === "undefined") {
+    String.prototype.replaceAll = function (match, replace) {
+      return this.replace(new RegExp(match, "g"), () => replace);
+    };
+  }
+
   var style = `
   div.pd-controls {
     display: inline-block !important;
