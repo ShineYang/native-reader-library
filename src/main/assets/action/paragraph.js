9b24e7d3a68e9897bd98cb78f62e2c42fd4a8dc9
kevinzhow <kevinchou.c@gmail.com>
2022-07-22 12:55:46 +0800

    fix(replaceAll): add polyfill

--- src/main/assets/action/paragraph.js
+++ src/main/assets/action/paragraph.js
@@ -1,3 +1,8 @@
+if (typeof String.prototype.replaceAll === "undefined") {
+  String.prototype.replaceAll = function (match, replace) {
+    return this.replace(new RegExp(match, "g"), () => replace);
+  };
+}
 /**
  * Request Host app to mark paragraph as read
  */
